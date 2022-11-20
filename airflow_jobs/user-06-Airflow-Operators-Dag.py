504e83d1c35e4dd675eb586a774d78eb63ae3336
pdefusco <pauldefusco@gmail.com>
2022-11-19 19:30:46 -0800

    finalized script

--- airflow_jobs/user-06-Airflow-Operators-Dag.py
+++ airflow_jobs/user-06-Airflow-Operators-Dag.py
@@ -1,8 +1,8 @@
 # Airflow DAG
 from cloudera.cdp.airflow.operators.cde_operator import CDEJobRunOperator
 from airflow.operators.bash import BashOperator
-#from airflow.operators.python_operator import PythonOperator
-#from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.http_operator import SimpleHttpOperator
 from datetime import datetime, timedelta
 from dateutil import parser
 from airflow import DAG
@@ -16,14 +16,14 @@ default_args = {
     'owner': "pauldefusco",
     'retry_delay': timedelta(seconds=30),
     'depends_on_past': False,
-    'start_date': datetime(2022,11,22,8),
+    'start_date': datetime(2022,11,18,1),
     'end_date': datetime(2023,9,30)
 }
 
-dag_name = '{}-06-airflow-tour-dag_2'.format(username)
+dag_name = '{}-06-airflow-tour-dag_3'.format(username)
 
 airflow_tour_dag = DAG(
-        dag_name,
+        'dagging3',
         default_args=default_args,
         schedule_interval='@yearly',
         catchup=False,
@@ -41,7 +41,7 @@ shell_step2 = BashOperator(
         dag=airflow_tour_dag,
         bash_command='echo "Hello Airflow" '
         )
-'''
+
 shell_jinja_step3 = BashOperator(
     task_id='bash_with_jinja',
     dag=airflow_tour_dag,
@@ -86,5 +86,5 @@ random_joke_step6 = PythonOperator(
     python_callable=_print_random_joke,
     dag=airflow_tour_dag
 )
-'''
-spark_sql_step1 >> shell_step2 # >> shell_jinja_step3 #>> print_context_step4 >> http_task_step5 >> random_joke_step6
+
+spark_sql_step1 >> shell_step2 >> shell_jinja_step3 #>> print_context_step4 >> http_task_step5 >> random_joke_step6
